c246500845fb909368a9a1eeea60a63153406917
Slayer2084 <90677284+Slayer2084@users.noreply.github.com>
2021-11-02 22:37:04 +0100

    Add files via upload

--- BadLabel.py
+++ BadLabel.py
@@ -1,9 +1,8 @@
 import pandas
-from CombineDatasets import get_combined_dataset
 from sklearn.utils import shuffle
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.linear_model import LogisticRegression, PassiveAggressiveClassifier
+from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, GradientBoostingClassifier
 from sklearn.naive_bayes import GaussianNB, MultinomialNB
@@ -19,11 +18,13 @@ class CorrectLabels:
                  index_column_name: str,
                  epochs: int,
                  threshold,
+                 preprocessing_pipe,
                  repeats: int = 500,
                  split_rate: int = 4,
                  ):
         self.dataset = dataset
         self.threshold = threshold
+        self.preprocessing_pipe = preprocessing_pipe
         self.label_column_name = label_column_name
         self.index_column_name = index_column_name
         self.repeats = repeats
@@ -33,24 +34,31 @@ class CorrectLabels:
 
     @staticmethod
     def shuffle_dataset(dataset):
+        print("Shuffling Data...")
         shuffled_dataset = shuffle(dataset)
         return shuffled_dataset
 
     def get_train_test(self, dataset):
-        X = dataset.drop(self.label_column_name)
-        y = dataset[self.index_column_name]
+        X = dataset.drop([self.label_column_name, self.index_column_name], axis="columns")
+        y = dataset[self.label_column_name]
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=(1/self.split_rate), random_state=7)
+        # print(X_train.shape, y_train.shape)
+        self.preprocessing_pipe.fit(X)
+        X_train, X_test = self.preprocessing_pipe.transform(X_train), self.preprocessing_pipe.transform(X_test)
+        print("Transforming Data...")
+        # print(X_train.shape, y_train.shape)
         return X_train, X_test, y_train, y_test
 
     @staticmethod
-    def form_models(self):
+    def form_models():
         models = {}
-        models["LR"] = LogisticRegression()
-        # models["PasAgr"] = PassiveAggressiveClassifier() No predict_proba
-        models["KNN"] = KNeighborsClassifier()
+        print("Forming Models...")
+        models["LR"] = LogisticRegression(n_jobs=-1)
+        # models["PasAgr"] = PassiveAggressiveClassifier(n_jobs=-1) No predict_proba
+        models["KNN"] = KNeighborsClassifier(n_jobs=-1)
         models["SVM"] = SVC()
         models["DCT"] = DecisionTreeClassifier()
-        models["RFC"] = RandomForestClassifier()
+        models["RFC"] = RandomForestClassifier(n_jobs=-1)
         models["ABC"] = AdaBoostClassifier()
         models["GBC"] = GradientBoostingClassifier()
         models["GNB"] = GaussianNB()
@@ -60,9 +68,11 @@ class CorrectLabels:
 
     def get_trained_models(self, X_train, y_train):
         fitted_models = {}
-        for model_name, model in self.models.items():
-            model.fit(X_train, y_train)
+        print("Starting to train models...")
+        for idx, (model_name, model) in enumerate(self.models.items()):
+            model.fit(X_train.toarray(), y_train)
             fitted_models[model_name] = model
+            print("Successfully trained ", model_name, " only ", (len(self.models) - idx), " more to go!")
         return fitted_models
 
     def get_predict(self, X_test, fitted_models: dict):
@@ -74,12 +84,35 @@ class CorrectLabels:
             preds[model_name] = (predict, mask)
         return preds
 
-    def repeat(self, dataset):
+    def repeat(self):
+        dataset = self.dataset
+        results = []
         for i in range(self.repeats):
             shuffled_dataset = self.shuffle_dataset(dataset)
             X_train, X_test, y_train, y_test = self.get_train_test(shuffled_dataset)
             fitted_models = self.get_trained_models(X_train, y_train)
             predictions = self.get_predict(X_test, fitted_models)
+            results.append(predictions)
+        for prediction in results:
+            print(prediction)
 
+
+if __name__ == "__main__":
+    from CombineDatasets import get_combined_dataset
+    from sklearn.feature_extraction.text import TfidfVectorizer
+    from sklearn.compose import ColumnTransformer
+
+    df = get_combined_dataset()
+    df["index"] = df.index
+    df = df[["index", "content", "label"]]
+
+
+    pipe = ColumnTransformer([
+        ("vec", TfidfVectorizer(stop_words="english", ngram_range=[1, 3], strip_accents=None,
+                                      lowercase=False, smooth_idf=False, analyzer="char", use_idf=True,
+                                      sublinear_tf=True, norm="l2", binary=True), "content")
+    ], remainder="passthrough")
+    label_corrector = CorrectLabels(df, "label", "index", 10, 0.90, pipe, repeats=1)
+    label_corrector.repeat()
 
 
